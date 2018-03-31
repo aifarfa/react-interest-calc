d92874778fc22a4be60b2a7357a45baf527fac82
aifarfa <kzneko@gmail.com>
2018-04-01 00:01:25 +0700

    implements range timeline calculation

--- src/services/interest-utils.js
+++ src/services/interest-utils.js
@@ -1,5 +1,9 @@
 import { Decimal } from 'decimal.js-light';
 
+export const add = (a, b) => {
+  return new Decimal(a).add(b).toNumber();
+};
+
 export const getYearInterest = (principal, rate) => {
   const interest = new Decimal(principal).mul(rate).div(100);
 
@@ -13,10 +17,9 @@ export const getMonthInterest = (principal, rate) => {
 };
 
 export const calculateNextMonth = (rate, isCompound) => previous => {
-  // console.log('calculateNextMonth', previous);
   const number = previous.number + 1;
   const principal = previous.principal;
-  const balance = previous.balance + previous.interest;
+  const balance = add(previous.balance, previous.interest);
   const interest = getMonthInterest(principal, rate);
 
   return { number, principal, balance, interest };
@@ -25,4 +28,27 @@ export const calculateNextMonth = (rate, isCompound) => previous => {
 export const getNextMonthSimple = rate => {
   const simpleInterest = month => false; // never add compound
   return calculateNextMonth(rate, simpleInterest);
+};
+
+export const getSimpleInterestTimeline = month => (principal, rate) => {
+  // init timeline
+  const range = Array(month - 1).keys(); // index
+  const timeline = Array.from(range); // [...range] spread 1,2,3..
+  const getNext = getNextMonthSimple(rate);
+  // initial values
+  const firstMonth = {
+    number: 1,
+    principal: principal,
+    balance: principal,
+    interest: getMonthInterest(principal, rate)
+  };
+  const result = [firstMonth];
+
+  return timeline.reduce((list, index) => {
+    const previous = list[index];
+    const nextMonth = getNext(previous);
+    list.push(nextMonth);
+
+    return list;
+  }, result);
 };
--- src/services/interest-utils.test.js
+++ src/services/interest-utils.test.js
@@ -1,11 +1,17 @@
 import {
   getYearInterest,
   getMonthInterest,
-  getNextMonthSimple
+  getNextMonthSimple,
+  getSimpleInterestTimeline
 } from './interest-utils';
 
 describe('interest utils', () => {
-  describe('getMonthInterest', () => {
+  describe('getYearInterest', () => {
+    it('get year interest rate 4', () => {
+      const interest = getYearInterest(1000, 4);
+      expect(interest).toEqual(40);
+    });
+
     it('get year interest rate 3.3', () => {
       const interest = getYearInterest(1200, 3.3);
       expect(interest).toEqual(39.6);
@@ -14,6 +20,13 @@ describe('interest utils', () => {
     it('get year interest rate 2.5', () => {
       const interest = getYearInterest(1200, 2.5);
       expect(interest).toEqual(30);
+    });
+  });
+
+  describe('getMonthInterest', () => {
+    it('get monthly round at precision 20', () => {
+      const interest = getMonthInterest(1000, 4);
+      expect(interest).toEqual(3.3333333333333335);
     });
 
     it('get monthly interest rate 3.3', () => {
@@ -30,16 +43,16 @@ describe('interest utils', () => {
   describe('getNextMonth - simple', () => {
     const principal = 1200;
     const rate = 3.3;
+    const interest = getMonthInterest(principal, rate);
+    const previous = {
+      number: 1,
+      principal: principal,
+      balance: principal,
+      interest: interest
+    };
     let result;
 
     beforeEach(() => {
-      const interest = getMonthInterest(principal, rate);
-      const previous = {
-        number: 1,
-        principal,
-        balance: principal,
-        interest
-      };
       const getNext = getNextMonthSimple(rate);
       result = getNext(previous);
       // console.log('result', result);
@@ -59,6 +72,51 @@ describe('interest utils', () => {
 
     it('add interest', () => {
       expect(result.interest).toEqual(3.3);
+    });
+  });
+
+  describe('getSimpleInterestTimeline', () => {
+    const principal = 1000;
+    const getTimeline = getSimpleInterestTimeline(15); // months
+
+    describe('getTimeline round precision', () => {
+      const rate = 4;
+      let actual;
+
+      beforeEach(() => {
+        actual = getTimeline(principal, rate);
+      });
+
+      it('reduce range', () => {
+        expect(actual.length).toEqual(15);
+      });
+
+      it('round precision (40 / 12 * 12)', () => {
+        // (40 / 12) * 12
+        expect(actual[12]['balance']).toEqual(1040);
+      });
+
+      it('sum up balance', () => {
+        // (40 / 12) * 12
+        expect(actual[14]['balance']).toEqual(1046.6666666666667);
+      });
+    });
+
+    describe('different rate', () => {
+      const rate = 4.2;
+      let actual;
+
+      beforeEach(() => {
+        actual = getTimeline(principal, rate);
+      });
+
+      it('reduce range', () => {
+        expect(actual.length).toEqual(15);
+      });
+
+      it('sum up balance', () => {
+        expect(actual[14]['balance']).toEqual(1049);
+      });
     });
   });
 });
