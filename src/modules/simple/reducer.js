4558d8c0fa130f6a1645e795e77086650dd31a83
aifarfa <kzneko@gmail.com>
2018-04-02 22:35:37 +0700

    implement inputs validation

--- src/modules/simple/reducer.js
+++ src/modules/simple/reducer.js
@@ -6,75 +6,71 @@ const initialState = Immutable.fromJS({
   principal: 1200,
   rate: 5,
   timePeriod: 12,
-  hasErrors: false
-})
+  hasErrors: false,
+  errors: {}
+});
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case t.RESET:
-      return initialState
+      return initialState;
 
     case t.SET_PRINCIPAL:
-      return setPrincipal(state, action.payload)
+      return setPrincipal(state, action.payload);
 
     case t.SET_RATE:
-      return setRate(state, action.payload)
+      return setRate(state, action.payload);
 
     case t.SET_TIME_PERIOD:
-      return setTimePeriod(state, action.payload)
+      return setTimePeriod(state, action.payload);
 
-    default: return state
+    default:
+      return state;
   }
-}
+};
 
 function setPrincipal(state, payload) {
-  const setState = stateNumberSetter(state, isPositive)
-  const nextState = setState('principal', payload.value)
-  return nextState
+  const setState = stateNumberSetter(state, isPositive);
+  const nextState = setState('principal', payload.value);
+  return nextState;
 }
 
 function setRate(state, payload) {
-  const setState = stateNumberSetter(state, isPercentage)
-  const nextState = setState('rate', payload.value)
-  return nextState
+  const setState = stateNumberSetter(state, isPercentage);
+  const nextState = setState('rate', payload.value);
+  return nextState;
 }
 
 function setTimePeriod(state, payload) {
-  const setState = stateNumberSetter(state, isPositiveInt)
-  const nextState = setState('timePeriod', payload.value)
-  return nextState
+  const setState = stateNumberSetter(state, isPositiveInt);
+  const nextState = setState('timePeriod', payload.value);
+  return nextState;
 }
 
 /**
- * curry func for state setter
+ * curry function for state setter
  */
 const stateNumberSetter = (state, validate) => (key, value) => {
-  // default
-  if (!value) {
-    return state.set(key, 0)
-  }
-
-  const number = parseFloat(value)
-  // validate
-  if (!number || !validate(number)) {
-    // console.log('invalid number', number)
-    return state
-      .set(key, value)
-      .set('hasErrors', true)
-  }
-
-  return state.set(key, value)
-}
+  const number = parseFloat(value);
+  const error = !number || !validate(number);
+  const next = state.set(key, value).setIn(['errors', key], error);
+  return next.set('hasErrors', hasErrors(next));
+};
 
 function isPositive(value) {
-  return value > 0
+  return value > 0;
 }
 
 function isPercentage(value) {
-  return value >= 0 && value <= 100
+  return value >= 0 && value <= 100;
 }
 
 function isPositiveInt(value) {
-  const num = new Decimal(value)
-  return num.isPositive() && num.isInteger()
+  const num = new Decimal(value);
+  return num.isPositive() && num.isInteger();
+}
+
+function hasErrors(state) {
+  const errors = state.get('errors').filter(e => e);
+  return errors.size > 0;
 }
--- src/modules/simple/reducer.test.js
+++ src/modules/simple/reducer.test.js
@@ -7,95 +7,112 @@ describe('simple/reducer - actions', () => {
   let state;
 
   beforeEach(() => {
-    state = reducer(undefined, { type: '@@INIT' })
-  })
+    state = reducer(undefined, { type: '@@INIT' });
+  });
 
   it('has default state', () => {
-    const action = { type: 'ANY' }
-    const state = reducer(undefined, action).toJS()
+    const action = { type: 'ANY' };
+    const state = reducer(undefined, action).toJS();
 
-    expect(state).toBeDefined()
-  })
+    expect(state).toBeDefined();
+  });
 
   it('reset state', () => {
-    const action = actions.reset()
-    const next = reducer(state, action).toJS()
+    const action = actions.reset();
+    const next = reducer(state, action).toJS();
 
     expect(next.principal).toEqual(1200);
     expect(next.rate).toEqual(5);
     expect(next.timePeriod).toEqual(12);
-  })
+  });
 
   it('setPrincipal', () => {
-    const action = actions.setPrincipal(777)
-    const next = reducer(state, action).toJS()
+    const action = actions.setPrincipal(777);
+    const next = reducer(state, action).toJS();
 
     expect(next.principal).toEqual(777);
-    expect(next.hasErrors).toBe(false);
-  })
+    expect(next.errors.principal).toBeFalsy();
+    expect(next.hasErrors).toBeFalsy();
+  });
 
   it('setPrincipal: NaN', () => {
-    const action = actions.setPrincipal('abc0')
-    const next = reducer(state, action).toJS()
+    const action = actions.setPrincipal('abc0');
+    const next = reducer(state, action).toJS();
 
-    expect(next.hasErrors).toBe(true);
-  })
+    expect(next.errors.principal).toBeTruthy();
+    expect(next.hasErrors).toBeTruthy();
+  });
 
   it('setPrincipal: negative', () => {
-    const action = actions.setPrincipal('-1')
-    const next = reducer(state, action).toJS()
+    const action = actions.setPrincipal('-1');
+    const next = reducer(state, action).toJS();
 
-    expect(next.hasErrors).toBe(true);
-  })
+    expect(next.errors.principal).toBeTruthy();
+    expect(next.hasErrors).toBeTruthy();
+  });
 
   it('setRate', () => {
-    const action = actions.setRate(15.5)
-    const next = reducer(state, action).toJS()
+    const action = actions.setRate(15.5);
+    const next = reducer(state, action).toJS();
 
     expect(next.rate).toEqual(15.5);
-    expect(next.hasErrors).toBe(false);
-  })
+    expect(next.errors.rate).toBeFalsy();
+    expect(next.hasErrors).toBeFalsy();
+  });
 
   it('setRate: NaN', () => {
-    const action = actions.setRate('-xyz')
-    const next = reducer(state, action).toJS()
+    const action = actions.setRate('-xyz');
+    const next = reducer(state, action).toJS();
 
-    expect(next.hasErrors).toBe(true);
-  })
+    expect(next.errors.rate).toBeTruthy();
+    expect(next.hasErrors).toBeTruthy();
+  });
 
   it('setRate: n > 100', () => {
-    const action = actions.setRate(100.01)
-    const next = reducer(state, action).toJS()
+    const action = actions.setRate(100.01);
+    const next = reducer(state, action).toJS();
 
-    expect(next.hasErrors).toBe(true);
-  })
-
+    expect(next.errors.rate).toBeTruthy();
+    expect(next.hasErrors).toBeTruthy();
+  });
 
   it('setRate: negative', () => {
-    const action = actions.setPrincipal('-1')
-    const next = reducer(state, action).toJS()
+    const action = actions.setRate(-1);
+    const next = reducer(state, action).toJS();
 
-    expect(next.hasErrors).toBe(true);
-  })
+    expect(next.errors.rate).toBeTruthy();
+    expect(next.hasErrors).toBeTruthy();
+  });
 
   it('setTimePeriod: valid', () => {
-    const action = actions.setTimePeriod(18)
-    const next = reducer(state, action).toJS()
+    const action = actions.setTimePeriod(18);
+    const next = reducer(state, action).toJS();
 
-    expect(next.hasErrors).toBe(false);
-  })
+    expect(next.errors.timePeriod).toBeFalsy();
+    expect(next.hasErrors).toBeFalsy();
+  });
 
   it('setTimePeriod: is float', () => {
-    const action = actions.setTimePeriod(5.5)
-    const next = reducer(state, action).toJS()
+    const action = actions.setTimePeriod(5.5);
+    const next = reducer(state, action).toJS();
 
-    expect(next.hasErrors).toBe(true);
-  })
+    expect(next.errors.timePeriod).toBeTruthy();
+    expect(next.hasErrors).toBeTruthy();
+  });
 
   it('setTimePeriod: negative', () => {
-    const action = actions.setTimePeriod(-2)
-    const next = reducer(state, action).toJS()
+    const action = actions.setTimePeriod(-2);
+    const next = reducer(state, action).toJS();
 
-    expect(next.hasErrors).toBe(true);
-  })
-})
+    expect(next.errors.timePeriod).toBeTruthy();
+    expect(next.hasErrors).toBeTruthy();
+  });
+
+  it('setTimePeriod: zero', () => {
+    const action = actions.setTimePeriod(0);
+    const next = reducer(state, action).toJS();
+
+    expect(next.errors.timePeriod).toBeTruthy();
+    expect(next.hasErrors).toBeTruthy();
+  });
+});
