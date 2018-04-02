5b7a66148e48995ac0b66831a56d3d2dd24d5c5c
aifarfa <kzneko@gmail.com>
2018-04-02 14:43:10 +0700

    setup react-redux + immutable

--- src/index.js
+++ src/index.js
@@ -6,5 +6,25 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as Immutable from 'immutable';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { reducer as rootReducer } from './modules'; // index
+
+// redux setup
+const initialState = Immutable.fromJS({
+  compound: {},
+  simple: {}
+});
+
+const store = createStore(rootReducer, initialState);
+
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
