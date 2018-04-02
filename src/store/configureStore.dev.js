a51facf81525514d15341a1249febc2b91b70e15
Pasit Rattanathammakool <kzneko@gmail.com>
2018-04-02 15:11:08 +0700

    Merge branch 'master' into render-testing

---
+++ src/store/configureStore.dev.js
@@ -0,0 +1,24 @@
+import { createStore, compose } from 'redux';
+import { reducer as rootReducer } from '../modules';
+import DevTools from '../modules/devtools';
+
+const enhancer = compose(
+  DevTools.instrument()
+);
+
+const configureStore = (initialState) => {
+  // Note: only Redux >= 3.1.0 supports passing enhancer as third argument.
+  // See https://github.com/reactjs/redux/releases/tag/v3.1.0
+  const store = createStore(rootReducer, initialState, enhancer);
+
+  // Hot reload reducers (requires Webpack or Browserify HMR to be enabled)
+  if (module.hot) {
+    // module.hot.accept('../reducers', () =>
+    //   store.replaceReducer(require('../reducers')/*.default if you use Babel 6+ */)
+    // );
+  }
+
+  return store;
+}
+
+export default configureStore;
