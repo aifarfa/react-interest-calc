2f430c99ea6eb6f144aeda7ddc4c3b3b3bfe296a
aifarfa <kzneko@gmail.com>
2018-04-03 22:37:25 +0700

    connected component - map dispatch to props

--- src/modules/compound/containers/CompoundCalculator.js
+++ src/modules/compound/containers/CompoundCalculator.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-// import * as actions from '../actions';
+import * as actions from '../actions';
 import InterestCalculator from '../../../components/InterestCalculator';
 
 const mapState = state => {
@@ -16,12 +16,12 @@ const mapState = state => {
 
 const mapDispatch = dispatch => {
   return {
-    // onFrequencyChange: e => dispatch(actions.setFrequency(e.target.value)),
-    // onPrincipalChange: e => dispatch(actions.setPrincipal(e.target.value)),
-    // onRateChange: e => dispatch(actions.setRate(e.target.value)),
-    // onTimePeriodChange: e => dispatch(actions.setTimePeriod(e.target.value)),
-    // onReset: e => dispatch(actions.reset()),
-    // onSubmit: e => dispatch(actions.submit())
+    onFrequencyChange: e => dispatch(actions.setFrequency(e.target.value)),
+    onPrincipalChange: e => dispatch(actions.setPrincipal(e.target.value)),
+    onRateChange: e => dispatch(actions.setRate(e.target.value)),
+    onTimePeriodChange: e => dispatch(actions.setTimePeriod(e.target.value)),
+    onReset: e => dispatch(actions.reset()),
+    onSubmit: e => dispatch(actions.submit())
   };
 };
 
