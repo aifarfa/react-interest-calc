e970497db4bb65b9858ddedb5d1ab74d3bff8f15
aifarfa <kzneko@gmail.com>
2018-04-04 02:10:38 +0700

    add intro homepage

---
+++ src/modules/home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+export default props => (
+  <main>
+    <p className="intro">
+      This app implemented using React and redux with immutable state. Press
+      CTRL+H to see Redux dev tools logs.
+    </p>
+    <p className="intro">
+      Select <strong>Simple interest</strong> or{' '}
+      <strong>Compound interest</strong> from menu.
+    </p>
+  </main>
+);
