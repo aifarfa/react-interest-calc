2c2b95df760a4ac4b3e35ba442d12a95442bcb4c
aifarfa <kzneko@gmail.com>
2018-04-04 00:28:58 +0700

    hide dev tools by default

--- src/modules/devtools/index.js
+++ src/modules/devtools/index.js
@@ -6,11 +6,11 @@ import LogMonitor from 'redux-devtools-log-monitor';
 import DockMonitor from 'redux-devtools-dock-monitor';
 
 const DevTools = createDevTools(
-  <DockMonitor toggleVisibilityKey='ctrl-h'
-               changePositionKey='ctrl-q'
-               defaultIsVisible={true}
-               defaultPosition='bottom'>
-    <LogMonitor theme='tomorrow' />
+  <DockMonitor
+    toggleVisibilityKey="ctrl-h"
+    changePositionKey="ctrl-q"
+    defaultIsVisible={false}>
+    <LogMonitor theme="tomorrow" />
   </DockMonitor>
 );
 
